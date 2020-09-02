a3b1650e6856c8090dc76c3a73d1992bb3151f55
katenejack@gmail.com <katene@ot.otangarei.org>
2020-09-02 15:05:54 +1200

    figuring out how to display text on nutrition pyramid ;P

--- client/components/NutritionPyramid.jsx
+++ client/components/NutritionPyramid.jsx
@@ -4,12 +4,15 @@ export class NutritionPyramid extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          username: '',
-          hash: '',
-          success:'',
-          error:'',
+          fat:false,
+          milk:false,
+          meat:false,
+          vegetable:false,
+          fruit:false,
+          bread:false
         }
         this.highlight = this.highlight.bind(this)
+        this.unhighlight = this.unhighlight.bind(this)
       }
     
       componentDidMount() {
@@ -17,17 +20,28 @@ export class NutritionPyramid extends Component {
       }
     
       highlight(e) {
-        this.setState({ [e.target.id]: e.target.value })
+        this.setState({ [e.target.id]: true })
       }
 
+      unhighlight(e) {
+        this.setState({ [e.target.id]: false })
+      }
     
     render() {
         return (
             <div>
 
-              <div className="row justify-content-center">
-                <img className="hvr-grow" src="/utils/nutrition-pyramid/fat-crop.png" alt="fat" id="fat"/>
+              <div className="row justify-content-center food-container">
+                <img className="hvr-grow" src="/utils/nutrition-pyramid/fat-crop.png" alt="fat" id="fat" onMouseEnter={this.highlight} onMouseLeave={this.unhighlight}/>
+                {this.state.fat &&
+                <div className="centered">
+                  <h4>Fats, Oils & Sweets</h4>
+                  <p className="lead text-capitalize">Use sparingly</p>
+                  <p>Provides calories but few vitamins and minerals.</p>
+                </div>
+                }
               </div>
+
               <div className="row justify-content-center">
                   <img className="hvr-grow" src="/utils/nutrition-pyramid/milk-crop.png" alt="milk" id="milk"/>
                   <img className="hvr-grow" src="/utils/nutrition-pyramid/meat-crop.png" alt="meat" id="meat"/>
