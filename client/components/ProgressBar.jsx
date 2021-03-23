a1540ab7997e02549c8164f9263f19427bb25778
Katene Cotton <katene@ot.otangarei.org>
2021-03-23 16:56:48 +1300

    progress bar

--- client/components/ProgressBar.jsx
+++ client/components/ProgressBar.jsx
@@ -4,28 +4,43 @@ export class ProgressBar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          sessions: 20,
+          total: 20,
           month: new Date().getMonth(),
-          term:0
+          term:0,
+          attendance:0
         }
+        this.term = this.term.bind(this)
       }
     
       componentDidMount() {
-        
+        this.term()
+        this.attendance()
       }
       
-      attendance(){
+      componentDidUpdate(prevProps,prevState){
+        if(this.state.term != prevState.term){
+          this.setState({attendance:0})
+        }
+      }
 
+      attendance(){
+        const attendance = this.state.attendance
+        const total = this.state.total
+        return (attendance/total)*100
       }
 
       term(){
-        switch (this.state.month) {
-          case 2:
-            this.setState({})
-            break;
-        
-          default:
-            break;
+        const month = this.state.month
+        if(month == 1 || month == 2 ||month ==  3 ){
+          this.setState({term: 1})
+        }else if(month == 4||month == 5){
+          this.setState({term:2})
+        }else if(month == 6||month == 7||month == 8){
+          this.setState({term:3})
+        }else if(month == 9||month == 10||month == 11){
+          this.setState({term: 4})
+        }else{
+          this.setState({term:0})
         }
       }
     
@@ -33,8 +48,11 @@ export class ProgressBar extends Component {
     render() {
         return (
             <div>
+              <button type="button" className="btn btn-secondary" onClick={() => this.setState({attendance:this.state.attendance ++})}>attendance</button>
+              <button type="button" className="btn btn-secondary" onClick={() => this.setState({term:this.state.term ++})}>term</button>
+
               <div className="progress">
-                <div className="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress-bar w-75" role="progressbar" aria-valuenow={this.state.attendance} aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
         )
